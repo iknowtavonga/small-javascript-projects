379d71f8bbf3004d84b433312b8611ca3c3b811c
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-10-07 15:34:02 +0200

    completed basic working version

---
+++ even-numbers/main.js
@@ -0,0 +1,31 @@
+// First we create a function that allows us to add numbers to an array
+const numbers = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(`${numbers[i]} is an even number`);
+  } else {
+    console.log(`${numbers[i]} is an odd number`);
+  }
+}
